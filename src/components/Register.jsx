1a50c841a94b73ae5fc83a41d8fc070a68349b65
abhi7889 <abhishek131214@gmail.com>
2023-10-01 13:20:05 +0530

    boilerplate react setup

---
+++ src/components/Register.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Style.css';
+
+export default function Register() {
+  return (
+    <div className="register-page">
+      <div className="image-left">
+        <h1>Discover new things on Superapp</h1>
+      </div>
+      <div className="form-right">ss</div>
+    </div>
+  );
+}
